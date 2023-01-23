f606d7b9a1c70d0390419430afe92200c218bff9
lmoreau21 <61840342+lmoreau21@users.noreply.github.com>
2023-01-23 12:46:57 -0600

    Fixed Bugs
    May have a frame rate error

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let spritesheet;
 let spritedata;
 let score = 0;
+let totalClicks = 0;
 let timer = 30;
 let animation = [];
 let death = [];
@@ -15,14 +16,13 @@ function preload() {
 
 
 function setup() {
-  createCanvas(600, 300);
+  createCanvas(1600, 800);
   for (let i = 0; i < 11; i++) {
       let img = spritesheet.get(34*i, 0,34, 31);
       animation.push(img);
   }
   death.push(spritesheet.get(34*11, 0,34, 31));
   death.push(spritesheet.get(34*12, 0,34, 31));
-
 
  
   // for (let i = 0; i < 10; i++) {
@@ -33,21 +33,22 @@ function setup() {
 function draw() {
   background(200)
   if (timer == 0) {
-    textSize(28);
+    textSize(40);
     text("GAME OVER", width/2 - textWidth("GAME OVER")/2, height/2);
+    textSize(30);
+    text("Accuracy: "+int((score/totalClicks)*100)+"%", width/2 - textWidth("Accuracy: 98%")/2, height/2+50);
     gameOver=true;
   }
-  textSize(18);
+  textSize(24);
   text('Score: '+score, width-textWidth('Score: '+score)-10, 20);
   text('Time: '+timer, 10, 20);
   if (frameCount % 30 == 0 && timer > 0) {
-    bugs[bugCount] = new Sprite(animation, random(15, height-33), random(0.1, 0.2)+score*0.05, random()<0.5, death);
-    bugCount ++;
+    bugs[bugCount] = new Sprite(animation, random(15, height-33), random(.5, 1)+score*0.05, random()<.5, death);
+    bugCount++;
   }
   for (let bug of bugs) {
-    
-    bug.walk();
     bug.show();
+    bug.walk();
   }
   
   if (frameCount % 60 == 0 && timer > 0) {
@@ -56,6 +57,7 @@ function draw() {
 }
 
 function mouseClicked() {
+  totalClicks++;
   for (let bug of bugs) {
     bug.deathCheck();
   }
--- sprite.js
+++ sprite.js
@@ -1,25 +1,24 @@
-this.x = 40;
+
 class Sprite {
-  constructor(animation, y, speed, isR, death) {
-    this.x = 40;
-    if(isR)
-      this.x = -40;  
+  constructor(animation, y, speed, isRight, death) {
     this.y = y;
     this.animation = animation;
-    this.w = this.animation[0].width;
+    this.w = 50;
     this.len = this.animation.length;
     this.speed = speed;
     this.index = 0;
-    this.isRight = isR;
+    this.isRight = isRight ;
     this.death = death;
     this.killBug = false;
     this.trans = 255;
-    // if(this.isRight){
-    //   this.x = -this.w;
-    // }else if (!this.isRight){
-    //   this.x = -width-this.w;
-    //   console.log(this.x+" "+width);
-    // }
+    this.start = true;
+
+    if(this.isRight){
+      this.x = -this.w;
+    }else if (!this.isRight){
+      this.x = -width-this.w;
+      console.log(this.x+" "+width);
+    }
   }
 
   show() {
@@ -30,7 +29,8 @@ class Sprite {
     }
     if(!this.killBug && !gameOver){
       let index = floor(this.index) % this.len;
-      tint(255, );
+      tint(255, 255);
+      this.animation[index].resize(50,50);
       image(this.animation[index], this.x, this.y);
       
     }else if(this.killBug){
@@ -38,25 +38,32 @@ class Sprite {
         this.trans--;
       }
       tint(255, this.trans);
+      this.death[0].resize(50,50);
+      this.death[1].resize(50,50);
       image(this.death[0], this.x, this.y);
       image(this.death[1],this.x,this.y);
     }
   }
 
   walk() {
+    if(!this.isRight){
+      scale(-1, 1);
+    }else if(this.isRight){
+      scale(1,1)
+    }
     if(!this.killBug && !gameOver){
       this.index += this.speed;
       this.x += this.speed * 5;
-      if (this.x > width + 20 && this.isRight) {
+      if (this.x > width && this.isRight) {
         this.x = -this.w;
-      }else if(this.x >= -36 && !this.isRight){
-        this.x = -width-this.w-20;
+      }else if(this.x > this.w && !this.isRight){
+        this.x = -width-this.w;
       }
     }
   }
 
   deathCheck(){
-    if(!gameOver&&((mouseX > this.x && mouseX < this.x+this.w)||(mouseX < abs(this.x) && mouseX > abs(this.x)-this.w)) && (mouseY > this.y && mouseY < this.y+36) && !this.killBug){
+    if(!gameOver&&((mouseX > this.x && mouseX < this.x+this.w)||(mouseX < abs(this.x) && mouseX > abs(this.x)-this.w)) && (mouseY > this.y && mouseY < this.y+50) && !this.killBug){
       this.killBug = true;
       score++;
       console.log(this.x+" "+width);
